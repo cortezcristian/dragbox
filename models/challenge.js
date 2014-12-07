b422be9742cec6ae86fa9dee7a7f0957ac7e4500
Cristian Cortez <cortez.cristian@gmail.com>
2014-12-07 03:57:18 -0300

    Add rule method

--- models/challenge.js
+++ models/challenge.js
@@ -30,6 +30,43 @@ challengeSchema.pre("save", function(next) {
     next();
 });
 
+// ### Method: addRule
+//
+// Usage:
+//
+//     var challenge = new Challenge({name:"ch1"});
+//     challenge.addRule(rule, function(err, ch1){});
+//
+// | Param     | Type     | Description                                                 |
+// | :----     | :----    | :----                                                       |
+// | challenge | Rule     | Rule Object                                                 |
+// | cb        | function | Callback function that'll return error and Challenge object |
+//
+//
+challengeSchema.method("addRule", function(obj, cb) {
+    var challenge = this;
+    var r1 = false;
+    var position = (challenge.rules) ? challenge.rules.length : 0;
+    if ( obj instanceof Rule ) {
+        r1 = obj;
+    }
+    
+    if ( typeof r1 != "object" ) {
+       cb(new Error("Rule object was expected"), null); 
+    } else {
+        // Check if challenge already exists, in that case remove it
+        var existsPosition = challenge.rules.indexOf(r1); 
+        if( existsPosition !== -1 ) {
+            // if position get out of lenght it'll readjust to the last item
+            // in that case move it to a different position
+            challenge.rules.splice(existsPosition, 1);
+        }
+        
+        challenge.rules.splice(position, 0, r1);
+        challenge.save(cb);
+    }
+});
+
 // ### Method:
 challengeSchema.method("instanceMethod", function(param, cb) {
     var challenge = this;
--- test/unit/models/challenge-tests.js
+++ test/unit/models/challenge-tests.js
@@ -10,12 +10,14 @@ require('../../../utils/dbconnect');
 
 // Global Variables for the test case
 var Challenge, challenge;
+var Rule, rule;
 
 // Unit Tests
 describe('Model Test Challenge', function(){
     before(function(){
         // Before all tests
         Challenge = require("../../../models/challenge.js");
+        Rule = require("../../../models/rule.js");
     });
 
     describe('Challenge', function(){
@@ -23,6 +25,11 @@ describe('Model Test Challenge', function(){
         it('add a challenge', function(done){
             challenge = new Challenge({ name: 'challenge'+Math.floor((Math.random() * 10) + 1)});
             challenge.save(done);
+        });
+
+        it('add a rule to a challenge', function(done){
+            rule = new Rule({ name: 'rule'+Math.floor((Math.random() * 10) + 1)});
+            challenge.addRule(rule, done);
         });
 
     });
